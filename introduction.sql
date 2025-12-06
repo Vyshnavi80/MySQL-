340f0518cf4010353072b870bd30a928ae3b0a11
Vyshnavi Tanikonda <vyshnavitanikonda@gmail.com>
2025-12-06 21:53:34 +0530

    Add files via upload

---
+++ introduction.sql
@@ -0,0 +1,8 @@
+create database instagram;
+use instagram;
+
+create table data(username varchar(233),firstname char(40),posts int,country char(50));
+select*from data;
+
+insert into data values ("_vyshu123","vyshnavi",3,"india"),
+("vyshu09","vyshu",4,"india");
