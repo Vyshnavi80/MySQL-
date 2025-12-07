a493d150b378df7f935f5a442672c61a81076c28
Vyshnavi Tanikonda <vyshnavitanikonda@gmail.com>
2025-12-07 23:07:32 +0530

    Update introduction.sql

--- introduction.sql
+++ introduction.sql
@@ -1,7 +1,9 @@
 create database instagram;
+
 use instagram;
 
 create table data(username varchar(233),firstname char(40),posts int,country char(50));
+
 select*from data;
 
 insert into data values ("_vyshu123","vyshnavi",3,"india"),
