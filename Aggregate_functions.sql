a24d53ad465bcf790ac5b5ad6d90b43672006c4b
Vyshnavi Tanikonda <vyshnavitanikonda@gmail.com>
2025-12-07 23:05:34 +0530

    Add files via upload

---
+++ Aggregate_functions.sql
@@ -0,0 +1,22 @@
+drop database if exists student;
+create database student;
+
+use student;
+
+create table data(name char(40),rollno int,branch char(20),section char(3),feedue int);
+
+select*from data;
+
+insert into data values("vyshu",10,"ECE","B",200),
+("Naveen",20,"CSE","A",300),
+("Navya",30,"CSE","C",100),
+("sruthi",40,"IT","B",300);
+
+select*from data where feedue>200 order by name;
+
+select count(name),branch from data group by branch having count(name);
+
+select min(feedue),max(feedue) from data;
+
+select avg(rollno),sum(feedue) from data;
+
