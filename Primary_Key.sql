71b54ce997c65624f06750af7eb44e63c0d2fd21
Vyshnavi Tanikonda <vyshnavitanikonda@gmail.com>
2025-12-12 11:39:32 +0530

    Create Primary_Key.sql

---
+++ Primary_Key.sql
@@ -0,0 +1,13 @@
+CREATE TABLE EMPLOYEE (
+  empId int primary key,
+  name varchar(15),
+  dept varchar(10)
+);
+
+-- insert
+INSERT INTO EMPLOYEE(empId,name,dept) VALUES (1, 'Clark', 'Sales');
+INSERT INTO EMPLOYEE(empId,name,dept) VALUES (2, 'Dave', 'Accounting');
+INSERT INTO EMPLOYEE(empId,name,dept) VALUES (3, 'Ava', 'Sales');
+
+-- fetch 
+SELECT * FROM EMPLOYEE;
