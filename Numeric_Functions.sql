64de6615f3f442ecc2210ea6e3ae1859fefd120c
Vyshnavi Tanikonda <vyshnavitanikonda@gmail.com>
2025-12-16 16:51:58 +0530

    Create Numeric_Functions.sql

---
+++ Numeric_Functions.sql
@@ -0,0 +1,43 @@
+-- NUMERIC FUNCTIONS
+
+-- abs
+select abs(-5);
+-- ceil
+select ceil(29.6);
+select ceil(20.1);
+-- floor
+select floor(27.8);
+-- round
+select round(20.1);
+select round(20.8);
+select round(20.5);
+-- truncate
+select truncate(2.456,2);
+-- power
+select power(2,3);
+-- square root
+select sqrt(16);
+-- exp(exponent)
+select exp(1);
+-- log
+select log(40);
+-- log10
+select log10(100);
+-- mod(gives remainder)
+select mod(5,2);
+-- sign
+select sign(-5);
+select sign(5);
+select sign(0);
+-- pi
+select pi();
+-- radians(converts degrees to radians)
+select radians(45);
+-- degrees
+select degrees(3.14159);
+-- gratest
+select gratest(10,20,30);
+-- least
+select least(10,20,30);
+-- rand
+select rand();
